fc92800e8df03a876ea9699302742493ab5ec8c4
Juni Kim <junikimm717@gmail.com>
2025-12-29 11:47:20 -0500

    build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+DIR="$(realpath "$(dirname "$0" )" )"
+
+cd "$DIR"
+
+mkdir -p "$DIR/dist"
+
+cp "$DIR/index.html" "$DIR/dist/"
+
+for i in $(seq 1 5); do (
+  echo "============================"
+  echo "compiling lecture $i..."
+  cd "lecture$i"
+  make clean
+  latexmk -pdf
+  for file in *.pdf; do
+    newfile="${file%.pdf}${i}.pdf"
+    mv "$file" "$DIR/dist/$newfile"
+  done
+) done
